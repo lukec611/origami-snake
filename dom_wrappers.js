3a773ba5d2b3ecc191b57491eb676b53e06740c2
luke lincoln <lukes611@gmail.com>
2019-05-15 21:12:05 +1000

    still working

---
+++ dom_wrappers.js
@@ -0,0 +1,22 @@
+
+class DomWrapper {
+    constructor(elementName, optionalStyles = {}) {
+        this.element = document.createElement(elementName);
+        this.setStyles(optionalStyles);
+    }
+    setStyles(styles) {
+        Object.entries(styles).forEach(([key, attrib]) => this.element.style[key] = attrib);
+    }
+    appendChild(child) {
+        this.element.appendChild(child);
+    }
+}
+
+class UpdatableDomWrapper {
+    constructor(elementName, optionalStyles) {
+        super(elementName, optionalStyles);
+    }
+    setHtml(str) {
+        this.element.innerHTML = str;
+    }
+}
